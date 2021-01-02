ae77c89452c8f344b1e4a7543ef00ce4f53ed43a
Göran Sander <goran@ptarmiganlabs.com>
2021-01-02 16:53:53 +0100

    First commit

---
+++ globals.js
@@ -0,0 +1,157 @@
+'use strict';
+
+const Influx = require('influx');
+const winston = require('winston');
+require('winston-daily-rotate-file');
+var config = require('config');
+const path = require('path');
+
+// Get app version from package.json file
+var appVersion = require('./package.json').version;
+
+// Set up logger with timestamps and colors, and optional logging to disk file
+const logTransports = [];
+
+logTransports.push(
+    new winston.transports.Console({
+        name: 'console',
+        level: config.get('EnergyMonitor.logLevel'),
+        format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.colorize(),
+            winston.format.simple(),
+            winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`),
+        ),
+    }),
+);
+
+if (config.get('EnergyMonitor.fileLogging')) {
+    logTransports.push(
+        new winston.transports.DailyRotateFile({
+            dirname: path.join(__dirname, config.get('EnergyMonitor.logDirectory')),
+            filename: 'EnergyMonitor.%DATE%.log',
+            level: config.get('EnergyMonitor.logLevel'),
+            datePattern: 'YYYY-MM-DD',
+            maxFiles: '30d',
+        }),
+    );
+}
+
+var logger = winston.createLogger({
+    transports: logTransports,
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`),
+    ),
+});
+
+// Function to get current logging level
+function getLoggingLevel() {
+    return logTransports.find(transport => {
+        return transport.name == 'console';
+    }).level;
+}
+
+// Get list of standard and user configurable tags
+// ..begin with standard tags
+let tagValuesEnergy = ['monitor_name', 'monitor_id', 'phases', 'firmware_version', 'firmware_revision'];
+let tagValuesMemory = ['instance_tag'];
+
+// Set up Influxdb client
+const influx = new Influx.InfluxDB({
+    host: config.get('EnergyMonitor.influxdbConfig.hostIP'),
+    port: `${config.has('EnergyMonitor.influxdbConfig.hostPort') ? config.get('EnergyMonitor.influxdbConfig.hostPort') : '8086'}`,
+    database: config.get('EnergyMonitor.influxdbConfig.dbName'),
+    username: `${config.get('EnergyMonitor.influxdbConfig.auth.enable') ? config.get('EnergyMonitor.influxdbConfig.auth.username') : ''}`,
+    password: `${config.get('EnergyMonitor.influxdbConfig.auth.enable') ? config.get('EnergyMonitor.influxdbConfig.auth.password') : ''}`,
+    schema: [
+        {
+            measurement: 'energy',
+            fields: {
+                a: Influx.FieldType.FLOAT,
+                v_ln: Influx.FieldType.FLOAT,
+                w: Influx.FieldType.FLOAT,
+                va: Influx.FieldType.FLOAT,
+                var: Influx.FieldType.FLOAT,
+                pf: Influx.FieldType.FLOAT,
+                hz: Influx.FieldType.FLOAT,
+
+                kwh_pos_tot: Influx.FieldType.FLOAT,
+                kvarh_pos_tot: Influx.FieldType.FLOAT,
+                kwh_neg_tot: Influx.FieldType.FLOAT,
+                kvarh_neg_tot: Influx.FieldType.FLOAT,
+                kw_dmd: Influx.FieldType.FLOAT,
+                kw_dmd_peak: Influx.FieldType.FLOAT,
+
+                kwh_pos_partial: Influx.FieldType.FLOAT,
+                kvarh_pos_partial: Influx.FieldType.FLOAT,
+                kwh_pos_t1: Influx.FieldType.FLOAT,
+                kwh_pos_t2: Influx.FieldType.FLOAT,
+            },
+            tags: tagValuesEnergy,
+        },
+        {
+            measurement: 'memory_usage',
+            fields: {
+                heap_used: Influx.FieldType.FLOAT,
+                heap_total: Influx.FieldType.FLOAT,
+                external: Influx.FieldType.FLOAT,
+                process_memory: Influx.FieldType.FLOAT,
+            },
+            tags: tagValuesMemory,
+        },
+    ],
+});
+
+function initInfluxDB() {
+    let dbName = config.get('EnergyMonitor.influxdbConfig.dbName');
+    let enableInfluxdb = config.get('EnergyMonitor.influxdbConfig.enableInfluxdb');
+
+    if (enableInfluxdb) {
+        influx
+            .getDatabaseNames()
+            .then(names => {
+                if (!names.includes(dbName)) {
+                    influx
+                        .createDatabase(dbName)
+                        .then(() => {
+                            logger.info(`CONFIG: Created new InfluxDB database: ${dbName}`);
+
+                            let newPolicy = config.get('EnergyMonitor.influxdbConfig.retentionPolicy');
+
+                            // Create new default retention policy
+                            influx
+                                .createRetentionPolicy(newPolicy.name, {
+                                    database: dbName,
+                                    duration: newPolicy.duration,
+                                    replication: 1,
+                                    isDefault: true,
+                                })
+                                .then(() => {
+                                    logger.info(`CONFIG: Created new InfluxDB retention policy: ${newPolicy.name}`);
+                                })
+                                .catch(err => {
+                                    logger.error(`CONFIG: Error creating new InfluxDB retention policy "${newPolicy.name}"! ${err.stack}`);
+                                });
+                        })
+                        .catch(err => {
+                            logger.error(`CONFIG: Error creating new InfluxDB database "${dbName}"! ${err.stack}`);
+                        });
+                } else {
+                    logger.info(`CONFIG: Found InfluxDB database: ${dbName}`);
+                }
+            })
+            .catch(err => {
+                logger.error(`CONFIG: Error getting list of InfuxDB databases! ${err.stack}`);
+            });
+    }
+}
+
+module.exports = {
+    config,
+    logger,
+    getLoggingLevel,
+    influx,
+    appVersion,
+    initInfluxDB,
+};
